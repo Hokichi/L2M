24b8fa381139b45dc2da4f1480985097450f9f1e
Linh Phạm <18520992@gm.uit.edu.vn>
2021-12-16 18:00:28 +0700

    Add project files.

---
+++ L2M/wwwroot/admin/js/audio-upload.js
@@ -0,0 +1,63 @@
+﻿function PreviewAudio(inputFile, previewElement) {
+    if (inputFile.files && inputFile.files[0] && $(previewElement).length > 0) {
+
+        $(previewElement).stop();
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            $(previewElement).attr('src', e.target.result);
+            var playResult = $(previewElement).get(0).play();
+
+            if (playResult !== undefined) {
+                playResult.then(_ => {
+                    // Automatic playback started!
+                    // Show playing UI.
+
+                    $(previewElement).show();
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+
+                        $(previewElement).hide();
+                        alert("File Is Not Valid Media File");
+                    });
+            }
+        };
+
+        reader.readAsDataURL(inputFile.files[0]);
+    }
+    else {
+        $(previewElement).attr('src', '');
+        $(previewElement).hide();
+        alert("File Not Selected");
+    }
+}
+
+//function Delete(url) {
+//    swal({
+//        title: "Are you sure?",
+//        text: "Once deleted, you will not be able to recover",
+//        icon: "warning",
+//        buttons: true,
+//        dangerMode: true
+//    }).then((willDelete) => {
+//        if (willDelete) {
+//            $.ajax({
+//                type: "DELETE",
+//                url: url,
+//                success: function (data) {
+//                    if (data.success) {
+//                        toastr.success(data.message);
+//                        dataTable.ajax.reload();
+//                    }
+//                    else {
+//                        toastr.error(data.message);
+//                    }
+//                }
+//            });
+//        }
+//    });
+//}
---
+++ L2M/wwwroot/admin/js/photo-upload.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(() => {
+    $('#photo').change(function () {
+        const file = this.files[0];
+        if (file) {
+            let reader = new FileReader();
+            reader.onload = function (event) {
+                console.log(event.target.result);
+                $('#imgPreview').attr('src', event.target.result);
+            }
+            reader.readAsDataURL(file);
+        }
+    });
+});
+
+
+//$(document).ready(() => {
+//    $("#audio").change(function () {
+//        const file = this.files[0];
+//        var sound = document.getElementById('sound');
+//        if (file) {
+//            let reader = new FileReader();
+//            reader.onload = function (event) {
+//                sound.src = event.target.result;
+//                sound.controls = true;
+//                sound.play();
+//            };
+//            reader.readAsDataURL(file);
+//        }
+//    });
+//});
+
+
+
+
