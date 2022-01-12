eade8ecf8aac368924a587e6da994587c4024563
Linh Phạm <18520992@gm.uit.edu.vn>
2022-01-12 17:55:47 +0700

    fix conflix

---
+++ L2M/Controllers/SongsController.cs
@@ -0,0 +1,23 @@
+﻿using L2M.Data;
+using L2M.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L2M.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnviroment;
+        private readonly L2MContext _context;
+        public SongsController(IWebHostEnvironment hostEnvironment, L2MContext context)
+        {
+            _context = context;
+            SongService.getContext();
+            this._webHostEnviroment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View(SongService.GetSong());
+        }
+    }
+}
