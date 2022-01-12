5419c65e5e6efd9450f27ce20e8b095e3d260520
kotori <wanbirosi1@gmail.com>
2022-01-12 19:39:03 +0700

    chỉnh Home

--- L2M/Controllers/HomeController.cs
+++ L2M/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using L2M.Services;
 
 namespace L2M.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+            BaseService.getContext();
+        }
         public IActionResult Index()
         {
+            ViewData["FeaturedSongs"] = SongService.GetFeaturedSongs();
+            ViewData["NewSongs"] = SongService.GetNewSongs();
+            ViewData["LikeSongs"] = SongService.GetMostLikedLikeSongs();
+            ViewData["TopSongs"] = SongService.GetTopSongs();
+            ViewData["Albums"] = AlbumService.GetAlbum();
             return View();
         }
     }
